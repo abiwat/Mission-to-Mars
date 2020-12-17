d5eaa9157e36e81264fd9df1dfb779ab52b90885
Abigail W Richardson <67278937+abiwat@users.noreply.github.com>
2020-12-17 17:03:19 -0600

    final push

--- app.py
+++ app.py
@@ -1,26 +1,33 @@
+# we'll use Flask to render a template
 from flask import Flask, render_template
+#use PyMongo to interact with our Mongo database
 from flask_pymongo import PyMongo
-import scraping
+#to use scraping code, convert jupyter notebook to python
+import scraping 
 
+
+#set up flask
 app = Flask(__name__)
 
 # Use flask_pymongo to set up mongo connection
-app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/mars_app'
 mongo = PyMongo(app)
 
-@app.route("/")
+
+#define route for html page
+@app.route('/')
 def index():
-   mars = mongo.db.mars.find_one()
-   return render_template("index.html", mars=mars)
+    mars = mongo.db.mars.find_one()
+    return render_template('index.html', mars=mars)
 
-@app.route("/scrape")
+#add scraping route
+@app.route('/scrape')
 def scrape():
-   mars = mongo.db.mars
-   mars_data = scraping.scrape_all()
-   mars.update({}, mars_data, upsert=True)
-   return "Scraping Successful!"
+    mars = mongo.db.mars
+    mars_data = scraping.scrape_all()
+    mars.update({}, mars_data, upsert=True)
+    return 'Scraping Successful!'
 
-.update(query_parameter, data, options)
-
+#code to run flask
 if __name__ == "__main__":
-   app.run()
+    app.run()
